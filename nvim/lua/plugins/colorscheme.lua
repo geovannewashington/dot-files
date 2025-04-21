5af2bc50935567908426f4e9822a4432f8fe12f3
Geovanne Washington <geoxp98@gmail.com>
2025-04-21 15:00:05 -0300

    Backup automático 2025-04-21 15:00:05

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,9 @@
 return {
-    'ellisonleao/gruvbox.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.o.background = "dark" -- or "light" for light mode
-        vim.cmd([[colorscheme gruvbox]])
-
-        -- Make background transparent
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-        vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-        vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
-    end
+  "morhetz/gruvbox", -- Use the original Gruvbox theme
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.o.background = "dark"
+    vim.cmd([[colorscheme gruvbox]])
+  end,
 }
-
